51af1e5333bae12a78f5312630bd7888f8bc4849
ChrisMH836 <223717319+ChrisMH836@users.noreply.github.com>
2026-07-20 15:20:24 -0500

    removed all files to restart project development

--- src/routers/testRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/test-route', (req, res) => {
-  res.json({ message: 'router test success' });
-});
-
-export default router;
